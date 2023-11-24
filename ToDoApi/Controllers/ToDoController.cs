cc3157c14183ea9a5cf4912a7298030b9ddeaab4
NaduHS <nadunariyapperuma@gmail.com>
2023-11-24 12:00:04 +0530

    added create, update and delete

--- ToDoApi/Controllers/ToDoController.cs
+++ ToDoApi/Controllers/ToDoController.cs
@@ -25,5 +25,75 @@ namespace ToDoApi.Controllers
 
             return result;
         }
+
+        //Get Item by Id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetTodoItemById(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        //	Create a new record
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> CreateTodoItem(ToDoItem item)
+        {
+            if(item.Id == 0)
+            {
+                _context.TodoItems.Add(item);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItemById), new { id = item.Id }, item);
+        }
+
+        // Mark an item as “done”
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(int id, ToDoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //	Delete records individually
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Delete all records
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAllTodoItems()
+        {
+            _context.TodoItems.RemoveRange(_context.TodoItems);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
